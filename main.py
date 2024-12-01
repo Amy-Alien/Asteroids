a3233212bef89e637ff78e785783f23daece5253
Amy-Alien <hughsence@gmail.com>
2024-12-01 17:44:38 +0400

    Add moving and groups

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
@@ -6,16 +7,30 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     clock = pygame.time.Clock()
-    dt = 0
     
+    updateble = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updateble, drawable)
+    
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        screen.fill("black")
-        pygame.display.flip()
+        for obj in updateble:
+            obj.update(dt)
         
+        screen.fill("black")
+        
+        for obj in drawable:
+            obj.draw(screen)
+        
+        pygame.display.flip()
+                
         # limit the framerate to 60FPS
         dt = clock.tick(60) / 1000
         
